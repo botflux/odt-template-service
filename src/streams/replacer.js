95c8e622f5295e91d2c9b0566f279d5fbe2296fe
Victor Mendele <victor.mendele68@gmail.com>
2019-03-13 14:48:30 +0100

    :star2:feat: replacer stream

---
+++ src/streams/replacer.js
@@ -0,0 +1,39 @@
+const { Transform } = require('stream')
+
+const makeReplacer = (context = {}) => {
+    const regex = /{{(\s)*(?<name>([a-z]|[A-Z]|[0-9])*)(\s)*}}/g
+
+    return new Transform ({
+        transform (chunk, encoding, callback) {
+            // console.log(chunk.toString(), encoding)
+
+            // Convert the buffer into a string
+            const str = chunk.toString()
+
+            // Replace variables
+            const result = 
+                (str.match(regex) || [])
+                .map(m => ({ 
+                    varName: m.replace(/{/g, '').replace(/}/g, '').trim(),
+                    varString: m
+                }))
+                .reduce((prev, { varName, varString }) => {
+
+                    if (!(varName in context)) {
+                        callback(`The variable ${varName} is not defined`)
+                    }
+
+                    return prev.replace(varString, context[varName])
+                }, str)
+                // .forEach(({ varName, varString }) => {
+                //     console.log(varName, varString)
+                // })
+            
+            // console.log(match, r.groups.name)
+
+            this.push(Buffer.from(result), encoding)
+        }
+    })
+}
+
+module.exports = makeReplacer

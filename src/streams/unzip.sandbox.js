873180029bbd0cdeb4254137d534fd8b0ff9a660
Victor Mendele <victor.mendele68@gmail.com>
2019-03-20 14:33:51 +0100

    test: sandbox example for modifying a odt file

--- src/streams/unzip.sandbox.js
+++ src/streams/unzip.sandbox.js
@@ -1,12 +1,55 @@
+const archiver = require('archiver')
+const fs = require('fs')
 const AdmZip = require('adm-zip')
+const zlib = require('zlib')
+const { DOMParser, XMLSerializer } = require('xmldom')
 
-const zip = new AdmZip('test/files/odt-file.odt')
+const archive = archiver ('zip')
+const zip = new AdmZip(
+    './test/files/test.odt'
+)
 
-let zipEntries = zip.getEntries()
+const myData = {
+    phone: '+33 698 765 432',
+    message: 'Helloooooooooooo world'
+}
 
-zipEntries.forEach(z => {
-    if (z.entryName === 'content.xml') {
-        console.log(z.getData().toString())
+archive.pipe(fs.createWriteStream('test/files/o.zip'))
+zip.getEntries().forEach(e => {
+
+    // console.log(e.entryName)
+    let opt = {
+        name: e.entryName,
     }
+
+    let content = e.getData()
+
+    if (e.entryName === 'mimetype') opt = {...opt, zlib: { level: zlib.Z_NO_COMPRESSION }}
+    else if (e.entryName === 'content.xml') {
+        const doc = new DOMParser().parseFromString (content.toString(), 'text/xml')
+
+        const elements = doc.getElementsByTagName('text:text-input')
+        // console.log(elements)
+
+        for (let i = 0; i < Number.parseInt(elements['$$length']); i++) {
+            const varName = elements[i.toString()].getAttribute('text:description')
+            // console.log(elements[i.toString()])
+            // console.log(varName)
+            if (varName in myData) {
+                elements[i.toString()].childNodes['0'].data = myData[varName]
+                console.log(elements[i.toString()].childNodes['0'].data)
+                // elements[i.toString()].data = myData[varName]
+            }
+        }
+
+
+        content = new XMLSerializer().serializeToString(doc)
+        console.log(content)
+    }
+
+    archive.append(content, opt)
 })
 
+// archive.append(fs.createReadStream('test/files/text-file.txt'), { name: 'text-file.txt' })
+// archive.append('Hello world', { name: 'Hello' })
+archive.finalize()
